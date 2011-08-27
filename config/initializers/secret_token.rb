6733c44f70413c35f85ab01e10edd9d54466369d
smoyer <smoyer@smoyer-desktop.(none)>
2011-08-26 19:57:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassThePuppy::Application.config.secret_token = 'db0c53f0c8a8e942ef76d63c927460f91e0c03f58fcef3dbc23177e673284db6e0b49a66002a7950b470c9eacd3b8aa7d1fb3cfa28fb4d161f12319eb3eea7d7'
