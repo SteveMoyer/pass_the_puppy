7e8da72c900bb08dff642d7f5d9befd12f29d4dd
Steve Moyer <smoyer@zynx.com>
2011-08-29 20:50:04 -0700

    added puppy images, refactored puppy controller

--- app/controllers/puppy_controller.rb
+++ app/controllers/puppy_controller.rb
@@ -1,11 +1,22 @@
 class PuppyController < ApplicationController
  View_Puppy_Path ='/puppy/view'
 
+ def get_puppy_status(puppy)
+  if puppy.owner
+    (puppy.owner ==current_user ? "You have ":puppy.owner.email+" has ") + "the puppy."
+  else
+    "The puppy is available."
+  end   
+ end
  def get_the_puppy()
      Puppy.find_by_id(1)
  end
  def view
   @puppy = get_the_puppy
+  @puppy_status = get_puppy_status(@puppy)
+  @puppy_comments = @puppy.comments
+  @can_take_puppy =  current_user && @puppy.owner==nil
+  @can_leave_puppy = current_user &&@puppy.owner &&(current_user==@puppy.owner ||current_user.is_admin)
  end
  def take
   @puppy =get_the_puppy
@@ -24,6 +35,7 @@ class PuppyController < ApplicationController
   @puppy=get_the_puppy
   if @puppy.owner==current_user || current_user.is_admin
    @puppy.owner=nil
+   @puppy.comments=nil 
    @puppy.save
    flash.now.alert = 'You returned the puppy.'
 
