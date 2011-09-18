8cefe211c909b2791f5f45f0ffeca13f980384b1
Steve Moyer <smoyer@zynx.com>
2011-09-18 16:43:53 -0700

    updated seed data, added change password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,21 @@ class UsersController < ApplicationController
      render "new"
     end
   end
-
+  def change_password
+        @user =current_user
+    if request.post? 
+      if @user.authenticate( params[:password][:old_password]) == @user 
+        @user.password = params[:password][:new_password] 
+        @user.password_confirmation = params[:password][:new_password_confirmation] 
+        if @user.save 
+          flash[:notice] = 'Your password has been changed' 
+          redirect_to '/change_password' 
+        else 
+          flash[:error] = 'Unable to change your password' 
+        end 
+      else 
+       flash[:error] = 'Invalid password' 
+      end 
+    end  
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-steveadmin =User.new(:email=>'smoyer@zynx.com', :password=>'zynx10880')
-steveadmin.is_admin=true
-steveadmin.save
-User.create!(:email=>'moyer.steve@gmail.com', :password=>'zynx10880', :is_admin=>false)
+admin =User.new(:email=>'admin@test.com', :password=>'adminpass')
+admin.is_admin=true
+admin.save
+User.create!(:email=>'nonadmin@test.com', :password=>'pass')
 
