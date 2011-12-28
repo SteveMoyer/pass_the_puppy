d40efe998e7d0751774f6d23dd35e41280e76f40
Steve Moyer <moyer.steve@gmail.com>
2011-12-27 23:03:54 -0800

    moving code from puppies_controller to model,changing views to use model directly

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,8 +1,8 @@
 class PuppiesController < ApplicationController
-
   def new 
     @puppy=Puppy.new
   end
+  
   def create
     @puppy = Puppy.new params[:puppy]
     if  @puppy.save
@@ -11,38 +11,37 @@ class PuppiesController < ApplicationController
       @errors= @puppy.errors 
       render :new
     end
-
   end 
+  
   def show
     @puppy = Puppy.find(params[:id])
   end
+  
   def index
     @puppylist=true
     @puppies = Puppy.all
   end 
+  
   def take
     @puppy =Puppy.find(params[:id])
-    if !@puppy.owner
-      @puppy.owner =current_user
+    if @puppy.can_take_puppy? current_user
       @puppy.update_attributes( params[:puppy])
-      @puppy.taken_time=Time.now
-      @puppy.save
-      flash.now.alert= 'You have taken the puppy'
+      @puppy.take(current_user)
+      if @puppy.save!
+        flash.now.alert= 'You have taken the puppy'
+      else
+        flash.alert='You didn\'t get the puppy'
+      end
     else
       flash.now.alert= 'The puppy is not available'
     end
     redirect_to @puppy
 
   end
+ 
   def leave
     @puppy=Puppy.find(params[:id])
-    if @puppy.owner==current_user || current_user.is_admin
-      puts @puppy.owner
-      ph=PuppyHistory.new
-      ph.update_attributes({:user => @puppy.owner,:taken_time=>@puppy.taken_time, :left_time=> Time.now,:take_comment=>@puppy.comments, :leave_comment=> params[:left_comment]})
-      @puppy.puppy_history<<ph
-      @puppy.owner=nil
-      @puppy.comments=nil 
+    if @puppy.leave current_user, params[:left_comment]
       @puppy.save
       flash.now.alert = 'You returned the puppy.'
     else
--- app/models/puppy.rb
+++ app/models/puppy.rb
@@ -1,20 +1,38 @@
 class Puppy <ActiveRecord::Base
- attr_accessible :comments,:name, :taken_time
- belongs_to :owner, :class_name =>"User"
- validates_uniqueness_of :name, :on=> :create
- has_many :puppy_history
-def status(user)
-  if owner
-    (owner ==user ? "You have ":owner.email+" has ") + "the puppy."
-  else
-    'The puppy is available' 
-end
-def is_available?
-  owner==nil
-end 
-def can_take_puppy?(user)
-  user!=nil
-end
-def can_leave_puppy?(user)
-  user && owner &&(user==owner ||user.is_admin)
+  attr_accessible :comments,:name, :taken_time
+  belongs_to :owner, :class_name =>"User"
+  validates_uniqueness_of :name, :on=> :create
+  has_many :puppy_history
+  def status(user=nil)
+    if owner
+      (owner ==user ? "You have ": owner.email+" has ") + "the puppy."
+    else
+      'The puppy is available' 
+    end
+  end
+  def is_available?
+    owner==nil
+  end 
+  def can_take_puppy?(user)
+    user && is_available?
+  end
+  def take(user)
+    self.owner=user
+    self.taken_time=Time.now
+  end
+  def can_leave_puppy?(user)
+    user && owner &&(user==owner ||user.is_admin)
+  end
+  def leave (user, left_comment)
+    if can_leave_puppy? user
+      ph=PuppyHistory.new
+      ph.update_attributes({:user => owner,:taken_time=>taken_time, :left_time=> Time.now,:take_comment=>comments, :leave_comment=> left_comment})
+      self.puppy_history<<ph
+      self.owner=nil
+      self.comments=nil 
+      true
+    else
+      false
+    end
+  end
 end
