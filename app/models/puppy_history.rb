91f4ee1e748bd737803e90a2ae77d98eb67a8911
Steve Moyer <moyer.steve@gmail.com>
2011-12-27 23:54:43 -0800

    adding display of current owner, time, and comment. Limiting history to 20 lines

--- app/models/puppy_history.rb
+++ app/models/puppy_history.rb
@@ -2,7 +2,7 @@ class PuppyHistory <ActiveRecord::Base
     belongs_to :puppy
     belongs_to :user
     attr_accessible :take_comment, :leave_comment, :taken_time,:left_time,:user
-    default_scope :order => "taken_time desc"
+    default_scope :limit=> 20,:order => "taken_time desc"
     def useremail
       user.email unless !user
     end 
