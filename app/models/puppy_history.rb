ed6f197cb623cf9745519c0144feeca568366f38
Steve Moyer <moyer.steve@gmail.com>
2013-02-25 20:49:27 -0800

    fixing useremail style

--- app/models/puppy_history.rb
+++ app/models/puppy_history.rb
@@ -3,7 +3,7 @@ class PuppyHistory <ActiveRecord::Base
     belongs_to :user
     attr_accessible :take_comment, :leave_comment, :taken_time,:left_time,:user
     default_scope :limit=> 20,:order => "taken_time desc"
-    def useremail
+    def user_email
       user.email unless !user
     end 
 end
